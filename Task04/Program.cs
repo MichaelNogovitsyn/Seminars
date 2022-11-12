1d050cd36591f8da05472af0b1a90796d09fc99b
mexatron <mexatron>
2022-11-12 21:19:25 +0200

    Task04 done

---
+++ Task04/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход три числа и 
+//выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Третье число");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+if (num3 > max) max = num3;
+if (num2 > max) max = num2;
+Console.WriteLine(max);
