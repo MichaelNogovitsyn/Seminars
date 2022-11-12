e7f28ee8ce9b07493327facfaa9f72a4cc3d844e
mexatron <mexatron>
2022-11-12 17:10:43 +0200

    task01 done

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Является ли первое число квадратом второго
+Console.WriteLine("Введите первое число");
+int num1 =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 =Convert.ToInt32(Console.ReadLine());
+int sqare = num2*num2;
+
+if(sqare==num1) Console.WriteLine("ДА");
+else Console.WriteLine("НЕТ");
+
