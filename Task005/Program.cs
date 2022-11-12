c2a99b96e61ec937e9b0d0a1cc1a28030f41ada1
mexatron <mexatron>
2022-11-12 20:07:58 +0200

    add task07

--- Task005/Program.cs
+++ Task005/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Программа которая на входе принимает число N, а на выходе
+//показывает все целые числа от -N до N.
+//4- -4,3,2,1,0,1,2,3,4
 Console.WriteLine("Hello, World!");
