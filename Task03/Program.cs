8e2e7da043ec753ac3db0104976f3610f30d3b07
mexatron <mexatron>
2022-11-12 20:04:24 +0200

    add task005

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -5,11 +5,11 @@ Console.WriteLine("Ведите номер дня от 1 до 7");
 
 int num = Convert.ToInt32(Console.ReadLine());
 
-if (num==1) Console.WriteLine("понедельник");
-else if (num==2) Console.WriteLine("вторник");
-else if(num==3)  Console.WriteLine("среда");
-else if (num==4) Console.WriteLine("четверг");
-else if (num==2) Console.WriteLine("пятница");
-else if (num==2) Console.WriteLine("суббота");
-else if (num==2) Console.WriteLine("воскресение");
+if (num == 1) Console.WriteLine("понедельник");
+else if (num == 2) Console.WriteLine("вторник");
+else if (num == 3) Console.WriteLine("среда");
+else if (num == 4) Console.WriteLine("четверг");
+else if (num == 2) Console.WriteLine("пятница");
+else if (num == 2) Console.WriteLine("суббота");
+else if (num == 2) Console.WriteLine("воскресение");
 else Console.WriteLine("не корректное число");
