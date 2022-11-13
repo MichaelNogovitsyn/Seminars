8b2cb012b9cad49d443ed3e74cce0fc71ac9135d
mexatron <mexatron>
2022-11-13 07:32:08 +0200

    Task06 Done

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число");
+int num=Convert.ToInt32(Console.ReadLine());
+if(num==0) Console.WriteLine("НЕТ"); // исключаем число 0;
+else if (num%2==0) Console.WriteLine("ДА");
+else Console.WriteLine("НЕТ");
