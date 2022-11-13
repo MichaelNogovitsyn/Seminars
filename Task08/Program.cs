49f20bed65369928c98433f2ffb5436e252097e6
mexatron <mexatron>
2022-11-13 07:44:10 +0200

    Task08 Done

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int count = 2; count <= N; count = count + 2)
+{
+    Console.WriteLine(count);
+}
