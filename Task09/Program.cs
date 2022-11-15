c18c3372cc61c62677b9c5816fe53a945aec38fc
mexatron <mexatron>
2022-11-15 19:36:07 +0200

    add task09 done

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа выводит случайное число от 10 -99
+//и находит максимаоьную цифру
+//76 -> 7
+int number = new Random().Next(10,100);
+Console.WriteLine($"Случайное число {number}");
+
+int firstDigit = number/10;
+int secondDigit = number%10;
+
+if ( firstDigit>secondDigit) Console. WriteLine($"наибольшая цифра числа {firstDigit}");
+else Console. WriteLine($"наибольшая цифра числа {secondDigit}");
