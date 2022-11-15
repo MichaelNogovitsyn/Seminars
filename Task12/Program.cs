f52a53574bce383118cf3e955d0fc2e39912603a
mexatron <mexatron>
2022-11-15 20:57:41 +0200

    task12 done

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа принимает на вход 2 числа и выводит, является ли первое число кратным второму
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool Multyplicity(int num1, int num2)
+{
+    if (num1 % num2 == 0) return true;
+    else return false;
+}
+bool result = Multyplicity(num1,num2);
+Console.WriteLine( result ? "Кратно" : "некратно");
