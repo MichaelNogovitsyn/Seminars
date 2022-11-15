c98dd2ec81a211dde61250145a9713947af93b7d
mexatron <mexatron>
2022-11-15 20:20:50 +0200

    add task11

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,11 +1,24 @@
 ﻿// Программа выводит случайное число от 10 -99
 //и находит максимаоьную цифру
 //76 -> 7
-int number = new Random().Next(10,100);
+int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число {number}");
 
-int firstDigit = number/10;
-int secondDigit = number%10;
+//int firstDigit = number / 10;
+//int secondDigit = number % 10;
 
-if ( firstDigit>secondDigit) Console. WriteLine($"наибольшая цифра числа {firstDigit}");
-else Console. WriteLine($"наибольшая цифра числа {secondDigit}");
+//if (firstDigit > secondDigit) Console.WriteLine($"наибольшая цифра числа {firstDigit}");
+//else Console.WriteLine($"наибольшая цифра числа {secondDigit}");
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"наибольшая цифра числа {maxDigit}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    //if (firstDigit > secondDigit) return firstDigit;
+    //else return secondDigit;
+    // аналог условия if
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+
