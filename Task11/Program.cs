0f8b8f91338e5b39bd6f99a2646bf30a78436f29
mexatron <mexatron>
2022-11-15 20:32:47 +0200

    task11 done

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Программа выводит случайное трехзначное число и удаляет вторую цифруч эото числа
 Console.WriteLine("Hello, World!");
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число{number}");
+
+int delaySecondDigit(int num)
+{
+    int firstDigit = num / 100;
+    int secondDigit = num % 10;
+    int digit = firstDigit * 10 + secondDigit;
+    return digit;
+}
+Console.WriteLine(delaySecondDigit(number));
