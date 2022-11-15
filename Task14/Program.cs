434d8b4d68f45a4528a592f6541753fc4ba86813
mexatron <mexatron>
2022-11-15 21:15:47 +0200

    task14 вщту

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа которая принимает на вход число и проверяет кратно ли оно одновременно 7 и 23
+Console.WriteLine("Введите число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Multyplicity7and23(int num)
+{
+    if (num % 7 == 0 && num % 23 == 0) return true;
+    else return false;
+}
+
+bool result=Multyplicity7and23(number);
+Console.WriteLine(result ? "Кратно" : "Некратно");
+
