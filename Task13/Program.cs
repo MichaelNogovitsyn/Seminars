d749a51faf9dcdcc894e0432d0b773242d14913a
mexatron <mexatron>
2022-11-16 20:14:52 +0200

    Task13 done

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 
+Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+ */
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int ThirdDigit(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("третьей цифры нет");
+        return -1;
+    }
+    // уменьшаем разряды числа до 3х.
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+
+    int result = number % 10;
+    return result;
+}
+Console.WriteLine(ThirdDigit(number));
