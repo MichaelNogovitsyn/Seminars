74b25b2d4da614904578225c556db123212a3233
mexatron <mexatron>
2022-11-16 20:55:24 +0200

    Task15 done

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным. */
+
+Console.WriteLine("Введите цифру соответствующюю дню недели от 1 до 7");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool weekend(int dayNumber)
+{
+    return dayNumber == 6 | dayNumber == 7 ? true : false;
+}
+
+bool result = weekend(number);
+Console.WriteLine(result ? "Да" : "Нет");
