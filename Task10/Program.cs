08981e492c6d2eeabd857684800246db8f0087ce
mexatron <mexatron>
2022-11-16 19:40:51 +0200

    Task10 done

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. 
+Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+ */
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SecondDigit(int number)
+{
+    int result = number / 10 % 10;
+    return result;
+}
+Console.WriteLine($"второе число {SecondDigit(number)}");
