1be27f2004c680b65071f3c6e55f25271fb680c6
mexatron <mexatron>
2022-11-18 21:16:26 +0200

    Task18 done

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа которая по заданному номеру четверти 
+//показывает диаппазон возможных координат в этой четверти 
+
+Console.WriteLine("Введите номер четверти");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string Quarter(int quaterNumber)
+{
+    if (quaterNumber == 1) return "x>0, y>0";
+    if (quaterNumber == 2) return "x<0, y>0";
+    if (quaterNumber == 3) return "x<0, y<0";
+    if (quaterNumber == 4) return "x>0, y<0";
+    else return "такой четверти нет";
+}
+
+//string result = Quarter(number);
+Console.WriteLine(Quarter(number));
