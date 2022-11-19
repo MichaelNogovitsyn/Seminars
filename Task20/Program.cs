3d073e3488470af27e8204dba9833f3cf811ae90
mexatron <mexatron>
2022-11-19 08:01:20 +0200

    Task20 done

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа принимает на входе координаты двух точек
+// на выходе находит расстояние между ними в 2D пространстве.
+Console.WriteLine("Введите координаты точки XА:");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки YA:");
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки XB:");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки YB:");
+int yb = Convert.ToInt32(Console.ReadLine());
+
+double Distantion(double xa, double ya, double xb, double yb)
+{
+    double distantX = xa-xb;
+    double distantY = ya-yb;
+    double squarX = distantX*distantX;
+    double squarY = distantY*distantY;
+    double result = Math.Sqrt(squarX+squarY);
+    return result;
+}
+double res = Distantion(xa,ya,xb,yb);
+double resRound = Math.Round(res,2, MidpointRounding.ToZero);
+Console.WriteLine(resRound);
