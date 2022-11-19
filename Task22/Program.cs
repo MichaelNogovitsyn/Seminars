643b718ac93c83f9153056077a74b63c3b15bb2d
mexatron <mexatron>
2022-11-19 08:17:23 +0200

    Task22 done

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа принимает на вход число N и выводит 
+// квадраты чисел от 1 до N;
+Console.Write("Введите число : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void tableOFsqrt(int n)
+{
+for (int i = 1; i <= n; i++)
+{
+  Console.WriteLine($"{i} -> {i*i} ");  
+}
+}
+
+tableOFsqrt(n);
