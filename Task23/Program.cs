90584f73b78cc3cd221257a288a4a3414b25086d
mexatron <mexatron>
2022-11-20 13:21:58 +0200

    Task23 done

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+5 ->
+1 | 1
+2 | 8
+3 | 27
+4 | 64
+5 | 125 */
+
+Console.Write("Введите число : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void tableOFcube(int n)
+{
+for (int i = 1; i <= n; i++)
+{
+  Console.WriteLine($"{i} -> {i*i*i} ");  
+}
+}
+
+tableOFcube(n);
