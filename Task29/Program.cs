40a0d8de3677b0dca25960abd6a8876daa7f28dc
mexatron <mexatron>
2022-11-24 21:41:47 +0200

    Task29 add

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33] */
+
+
+string RandomArray()
+{
+    int lengthArray = 8;
+    int[] array = new int[lengthArray];
+    Random rnd = new Random();
+    string str = "[";
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 100);
+        str = str + $"{array[i]},";
+    }
+    int end = str.Length - 1;
+    str=str.Remove(end);
+    return str + "]";
+}
+Console.WriteLine(RandomArray());
