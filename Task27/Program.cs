c9c7c147032701459b7da53affd1c0acd1a310de
mexatron <mexatron>
2022-11-24 20:53:55 +0200

    Task27 done

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12 */
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number<0) number= -number;
+
+int SumDigits(int num)
+{
+    int sumDigits = 0;
+    while (num > 0)
+    {
+        sumDigits = sumDigits + num % 10;
+        num = num / 10;
+    }
+    return sumDigits;
+}
+Console.WriteLine(SumDigits(number));
