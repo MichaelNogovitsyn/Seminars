6b396b6eb8e1a622f1f1c84610d34ff9494459a5
mexatron <mexatron>
2022-11-24 19:57:22 +0200

    Task25 add

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* // /* Программа выводит массив
+заполненный нулями и единицами в случайном порядке */ 
+
+Console.WriteLine("Enter array lengh ");
+int l= Convert.ToInt32(Console.ReadLine());
+
+int[] array= new int[l];
+Random rnd = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i]= rnd.Next(0,2);
+    Console.Write($"{array[i]} ");
+}
