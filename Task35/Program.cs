9ead157019b7479b21305f330062a3e9c6b412a8
mexatron <mexatron>
2022-11-27 07:44:40 +0200

    Task35 done

--- Task35/Program.cs
+++ Task35/Program.cs
@@ -1,3 +1,40 @@
 ﻿// задайте одномерный массив из 123 случайных чисел. 
 //Найдите количество элементов массива, значения которрых лежат в отрезке [10,99]
-Console.WriteLine("Hello, World!");
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+int CountRangeDigit(int[] arr,int min, int max)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > min && arr[i] < max) count++;
+    }
+    return count;
+}
+
+int countRangeDigit=0;
+int[] array = CreateArray(123, 0, 100);
+PrintArray(array);
+countRangeDigit=CountRangeDigit(array,10,99);
+Console.WriteLine(countRangeDigit);
