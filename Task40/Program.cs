d6f68f4d3933a88a50c8eaf6386bc80f234f2857
mexatron <mexatron>
2022-11-29 20:27:36 +0200

    Task 40 done

--- Task40/Program.cs
+++ Task40/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Принимает на вход 3 числа и проверяет, 
+//может ли сухествовать треугольник с сторонами такой длины
+Console.WriteLine("Введите сторону А:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону B:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону C:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+bool IsTriangle(int numA, int numB, int numC)
+{
+if (numA<numB+numC&&numB<numA+numC&&numC<numB+numA) return true;
+else return false;
+}
+System.Console.WriteLine(IsTriangle(a,b,c)?"Является" : "Нет" );
