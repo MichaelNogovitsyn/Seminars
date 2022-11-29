eac56c208a6b623a41d7bb79e56f03397436f6b8
mexatron <mexatron>
2022-11-29 21:22:10 +0200

    Task 42 done

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Преобразует десятичное число в двоичное
+Console.WriteLine("Введите десятичное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] Binary(int number)
+{
+    int digits = 0;
+    int numberB = number;
+    while (number > 0)
+    {
+        number /= 2;
+        digits += 1;
+    }
+    int[] result = new int[digits];
+
+    number = numberB;
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = number % 2;
+        number /= 2;
+    }
+    Array.Reverse(result);
+    return result;
+}
+int[] array = Binary(number);
+PrintArray(array);
