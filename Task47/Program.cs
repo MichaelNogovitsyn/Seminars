70ce07d982f4c7e5056da68ffbba3220b7396b77
mexatron <mexatron86@gmail.com>
2022-12-04 15:28:39 +0200

    Task47 add

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+
+double[,] CreateMatrixRndDouble(int rows, int columns, int min, int max)
+{
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(rnd.NextDouble()*(max-min)+min,2);
+        }
+    }
+    return matrix;
+}
+void PrintMatrixDouble(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1)-1) Console.Write($"{matrix[i,j],5}, ");
+            else Console.Write($"{matrix[i,j],4}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+double[,] array = CreateMatrixRndDouble(5,5,-2,10);
+PrintMatrixDouble(array);
