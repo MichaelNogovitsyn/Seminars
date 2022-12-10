d1cb2b875f1cd26df020e2efe69ea6e919edd8e9
mexatron <mexatron86@gmail.com>
2022-12-10 20:28:24 +0200

    Task63 done

--- Task63/Program.cs
+++ Task63/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа выводит числа от m до n
+
+void OrderNum(int m, int n)
+{
+    if (m < n)
+    {
+        if (m < n)
+        {
+            System.Console.WriteLine(m);
+            OrderNum(m + 1, n);
+        }
+    }
+
+    if (m > n)
+    {
+        if (n < m)
+        {
+            System.Console.WriteLine(m);
+            OrderNum(m - 1, n);
+        }
+    }
+    
+    if (m == n) System.Console.WriteLine(m);
+}
+
+OrderNum(10, 0);
