734a7d38dde4fc3c9885ddbb20c9cf158195938e
mexatron <mexatron86@gmail.com>
2022-12-10 21:08:51 +0200

    Task67 done

--- Task67/Program.cs
+++ Task67/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа которая принимает на вход число и возвращает сумму его цифр.
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumRec(int num)
+{
+    if (num / 10 > 0) 
+    {
+        return num % 10 + SumNumRec(num / 10);
+    }
+    return num % 10;
+}
+
+int sum = SumNumRec(number);
+System.Console.WriteLine(sum);
