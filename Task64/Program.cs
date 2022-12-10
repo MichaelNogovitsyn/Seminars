52fdb54ec6c4e44cb9c25bfa5068be5f2747edd7
mexatron <mexatron86@gmail.com>
2022-12-10 21:55:26 +0200

    Task64 done

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 64: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+System.Console.Write("Введите число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine();
+
+void NumToOrder(int m, int n)
+{
+    if (m < n)
+    {
+        if (m < n)
+        {
+            System.Console.Write($"{m},");
+            NumToOrder(m + 1, n);
+        }
+    }
+
+    if (m > n)
+    {
+        if (n < m)
+        {
+            System.Console.Write($"{m},");
+            NumToOrder(m - 1, n);
+        }
+    }
+    
+    if (m == n) System.Console.WriteLine($"{m}");
+}
+
+NumToOrder(numN, numM);
