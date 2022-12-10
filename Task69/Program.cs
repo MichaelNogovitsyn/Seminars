0a6fc346ae56b7641cd6b6daf411fa330f6d9a5e
mexatron <mexatron86@gmail.com>
2022-12-10 21:16:29 +0200

    Task69 add

--- Task69/Program.cs
+++ Task69/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+System.Console.WriteLine("Введите 1 число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите 2 число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int DegreeNumRec(int numA, int numB)
+{
+    if (numB>0) 
+    {
+        return numA*DegreeNumRec(numB-1);
+    }
+    return numA;
+}
+
+int degree=DegreeNumRec(numberA,numberB);
+System.Console.WriteLine(degree);
