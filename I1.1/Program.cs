ae12bd8ae0cd90edb2b27ad247fbefd0dc1b059c
mexatron <mexatron86@gmail.com>
2022-12-26 19:10:32 +0200

    I1.1 done

--- I1.1/Program.cs
+++ I1.1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* I1.1. Рассчитать значение у при заданном значении х:
+y=sin(x) при x>0 или y=cos(x) в противном случае. */
+System.Console.Write("Enter x:");
+double x = Convert.ToDouble(Console.ReadLine());
+double y = 0;
+if (x > 0)
+{
+    y = Math.Cos(x);
+}
+else { y = Math.Sin(x); }
+
+System.Console.WriteLine(y);
