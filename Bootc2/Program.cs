798c21788ae122d9621f69ca2a8e772497e5fda7
mexatron <mexatron86@gmail.com>
2023-01-09 20:02:54 +0200

    Bootc2 done

--- Bootc2/Program.cs
+++ Bootc2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Поиск максимума из 3х подряд чисел массива.
+int size=10;
+int m =3;
+int[] array = Enumerable.Range(1,size)
+                        .Select(item=> Random.Shared.Next(10))
+                        .ToArray();
+
+System.Console.WriteLine($"[{string.Join(", ", array)}]");
+
+int max=0;
+
+for (int i = 0; i < array.Length-m; i++)
+{
+    int t=0;
+    for (int j = i; j < i+m; j++)
+    {
+        t=t+array[j];
+    }
+    if (t>max) max=t;
+}
+System.Console.WriteLine(max);
