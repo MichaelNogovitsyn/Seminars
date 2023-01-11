5e7f19981605dea11a317efcb90ce4d6e523f652
mexatron <mexatron86@gmail.com>
2023-01-11 20:15:53 +0200

    metod1 done

---
+++ bootc3/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n=5;
+int[,] matrix = new int[n,n]; 
+Console.WriteLine("Hello, World!");
+for (int i = 0; i < n; i++)
+{
+    for (int j = i; j < n; j++)
+    {
+         matrix[i,j]=(i+j)*(j+1);
+         matrix[j,i]=(i+j)*(j+1);
+    }
+
+}
+
+//Print
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+         System.Console.Write(matrix[i,j]);
+         System.Console.Write("\t");
+    }
+System.Console.WriteLine();
+}
