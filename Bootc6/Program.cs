27af1da3d696a9f24e446eb4339bb266aebf22fb
mexatron <mexatron86@gmail.com>
2023-01-15 08:17:38 +0200

    bootc8 added

--- Bootc6/Program.cs
+++ Bootc6/Program.cs
@@ -1,18 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿/*
+1. arr = { 0, -5, 2, 3, 5, 9, -1, 7 };
+2. pivot = arr[6]  (опорный элемент)
+3. Вызвать шаги 1-2 для подмассива слева от pivot и справа от pivot
+*/
+int[] arr = { 0, -5, 2, 3, 5, 9, -1, 7 };
+QuickSort(arr, 0, arr.Length - 1);
+Console.Write($"Отсортированный массив {string.Join(", ", arr)}");
 
-namespace ConsoleApp5
+static void QuickSort(int[] inputArray, int minIndex, int maxIndex)
 {
-    partial class Program
+    if (minIndex >= maxIndex) return;
+    int pivot = GetPivotIndex(inputArray, minIndex, maxIndex);
+    QuickSort(inputArray, minIndex, pivot - 1);
+    QuickSort(inputArray, pivot + 1, maxIndex);
+    return;
+}
+static int GetPivotIndex(int[] inputArray, int minIndex, int maxIndex)
+{
+    //arr = { 0, -5, 2, 3, 5, 9, -1, 7 };
+    int pivotIndex = minIndex - 1; System.Console.WriteLine(minIndex - 1);
+    for (int i = minIndex; i <= maxIndex; i++)
     {
-        //0, 2, -5, 5, 3, 7, 9, -1
-        //0, 2, -5, 5, 3, 7, 9, -1
-        static void Main(string[] args)
+        if (inputArray[i] < inputArray[maxIndex])
         {
-            int[] arr = { 0, -5, 2, 3, 5, 9, -1, 7 };
-            QuickSort(arr, 0, arr.Length - 1);
-            Console.Write($"Отсортированный массив {string.Join(", ", arr)}");
+            pivotIndex++;
+            Swap(inputArray, i, pivotIndex);
+            System.Console.WriteLine(string.Join(',',inputArray));
         }
-        
+       
     }
+    pivotIndex++;
+    Swap(inputArray, pivotIndex, maxIndex);
+    System.Console.WriteLine($"pivotIndex = {pivotIndex}");
+    System.Console.Write("Swap2: ");
+    System.Console.WriteLine(string.Join(',',inputArray));
+    return pivotIndex;
+}
+static void Swap(int[] inputArray, int leftValue, int rightValue)
+{
+    int temp = inputArray[leftValue];
+    inputArray[leftValue] = inputArray[rightValue];
+    inputArray[rightValue] = temp;
 }
